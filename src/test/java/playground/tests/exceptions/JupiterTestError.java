20ba3c2b2c64115148b440cead7b008d4fa1058a
Vyacheslav Rusakov <vyarus@gmail.com>
2021-12-31 02:44:30 +0700

    fix tests

--- src/test/java/playground/tests/exceptions/JupiterTestError.java
+++ src/test/java/playground/tests/exceptions/JupiterTestError.java
@@ -1,14 +1,15 @@
 package playground.tests.exceptions;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import ru.vyarus.spock.jupiter.support.ActionHolder;
 import ru.vyarus.spock.jupiter.support.LifecycleExtension;
 
 /**
  * @author Vyacheslav Rusakov
  * @since 30.12.2021
  */
+@Disabled
 @ExtendWith(LifecycleExtension.class)
 public class JupiterTestError {
 
