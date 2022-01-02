45263b70eca2c6cabd73aa78b847ed2665718356
Vyacheslav Rusakov <vyarus@gmail.com>
2022-01-02 18:52:35 +0700

    fix tests

--- src/test/groovy/ru/vyarus/spock/jupiter/test/SpockCompetingParameterInjection.groovy
+++ src/test/groovy/ru/vyarus/spock/jupiter/test/SpockCompetingParameterInjection.groovy
@@ -1,15 +1,18 @@
 package ru.vyarus.spock.jupiter.test
 
 import org.junit.jupiter.api.extension.ExtendWith
+import ru.vyarus.spock.jupiter.AbstractTest
 import ru.vyarus.spock.jupiter.support.ActionHolder
 import ru.vyarus.spock.jupiter.support.ParameterExtension
 import ru.vyarus.spock.jupiter.support.ParameterExtension2
+import spock.lang.Requires
 import spock.lang.Specification
 
 /**
  * @author Vyacheslav Rusakov
  * @since 02.01.2022
  */
+@Requires({ AbstractTest.ACTIVE })
 @ExtendWith([ParameterExtension, ParameterExtension2])
 class SpockCompetingParameterInjection extends Specification {
 
